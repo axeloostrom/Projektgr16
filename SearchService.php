361cbacb6b8561049842704a8eb1c7a8474a12f4
Akseluhr <50402197+Akseluhr@users.noreply.github.com>
2019-05-22 11:08:38 +0200

    söktjänst

---
+++ SearchService.php
@@ -0,0 +1,42 @@
+<?php 
+
+
+
+?>
+
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+    </head>
+        <body>
+            <h1> Lediga tjänster </h1>
+             Jobbkategori: </br>
+            <select> 
+                <option value="Välj tjänst" id="abc" name="abc">Välj tjänst...</option>
+                <option value="Barnvakt" id="Barnvakt" name="Barnvakt">Barnvakt</option>
+                <option value="Hundvakt" id="Hundvakt" name="Hundvakt">Hundvakt</option>
+                <option value="Trädgårdstjänster" id="träd" name="träd">Trädgårdstjänster</option>
+                <option value="Handling" id="handla" name="handla">Handling</option>
+                <option value="Städning" id="Städning" name="Städning">Städning</option>
+                <option value="Flytthjälp" id="Flytt" name="Flytt">Flytt- och bärhjälp</option>
+                <option value="Nation" id="Nation" name="Nation">Nation</option>
+                <option value="To be continued" id="tbc" name="tbc">To be continued</option>
+            </select> </br>
+
+               </br> Region: </br>
+               <select>
+                <option value="Område" id="ab" name="ab">Välj område...</option>
+                <option value="Boländerna" id="bländer" name="bländer">Boländerna</option>
+                <option value="Luthagen" id="Lutis" name="Lutis">Luthagen</option>
+                <option value="Ekeby" id="Ekeby" name="Ekeby">Ekeby</option>
+                <option value="Rackarbergen" id="RackC" name="RackC">Rackarbergen</option>
+                <option value="Studentstaden" id="Stud" name="Stud">Studentstaden</option>
+                <option value="Centrum" id="C" name="C">Centrum</option>
+                <option value="To be continued" id="tb" name="tb">To be continued</option>
+            </select>
+            </br> </br> <button type="button">Sök</button>
+
+        </body>
+
+</html>
