4aa103e01f9387805d3d7409010ab86a075eec71
axeloostrom <50862882+axeloostrom@users.noreply.github.com>
2019-05-28 17:46:22 +0200

    UploadSERVICE
    <?php
    	include 'Include/DB.php';
    	authorization(); //Checks whether the Session variable "hashed_password" has been set.
    ?>
    <!Doctype html>
    <html>
    	<head>
    		<meta charset = "UTF-8">
    		<link rel = "stylesheet" type = "text/css"  href = "assets\css\css3.css" />
    		<link rel='stylesheet' type='text/css' href="assets\css\style.php" />
    		<!--Links to the external php & css-sheets that are being used.-->
    		<ul class="mstructure">
    			<li class="mblock"><a class="active" href="Index.php">Topics</a></li>
    			<li class="mblock"><a href="Logout-process.php">Logout</a></li>
    		</ul>
    
    		<title>Uppsala Annonstorg</title>
    			<h1 id="Big">Upp</h1>
    			<h1>The forum where hard topics are discussed constructively</h1>
    	</head>
    	<body onload='document.usrForm.usrname.focus()'>
    
    	<!--Allows the user to instantly write their name that they use when posting a comment-->
    		<div class="bar">
    			<h2 id="topic"> Topic #328: How might Industrial Countries help Developing ones most effectively?</h2>
    		</div>
    	 	<!--Start of Posting Form-->
    		<form action="Posts-Create.php" name="usrForm">
    			<fieldset id="field">
    				<div class="formId">Name:</div>
    					<input class="input" type="text" name="usrname" required><br> <!--First inputfield allowing user to enter the name which is linked to the comment-->
    				<div class="formId">Email:</div>
    					<input class="input" type="text" name="usremail" required><br><br> <!--Second inputfield allowing user to enter the email which is linked to the comment but not displayed-->
    				<div class="formId">Comment:</div>
    					<input id="commentbox" type "text" name="comment" required><br> <!--Third inputfield allowing user to write the actual comment-->
    				<input class="subbutton"type="submit" onclick="validateIndexEmail(document.usrForm.usremail)"> <!--Submitbutton sending the email to a javascript function which validates it-->
    			</fieldset>
    		</form>
    		<script src="assets\js\js.js"></script>
    		<!--Links to eternal javascriptfile in order to seperate code.-->
    		<!--End of Posting Form-->
    	</body>

--- UploadService-Process.php
+++ UploadService-Process.php
@@ -1,17 +1,7 @@
 <?php
-// Start the session
-session_start();
-
-	$uname = "dbtrain_850";
-	$pass = "rkdrha";
-	$host = "dbtrain.im.uu.se";
-	$dbname = "dbtrain_850";		
-
-	$connection = new mysqli( $host, $uname, $pass, $dbname);
-	if ($connection -> connect_error)
-	{
-		die ("Connection failed:".$connection.connect_error) ;
-	}
+include 'Include/DB.php';
+    $connection = dbconnect();
+	authorization();
 
 $adress = $connection -> real_escape_string($_POST['region']);
 $email = $connection -> real_escape_string($_POST['mail']);
@@ -26,11 +16,13 @@ $query = "INSERT INTO Prgr16_Jobs (Email, Job_Category, Adress, Est_Time, Wage, 
 
 $connection -> query ($query);
 //
- $title ='Annonssida.php3';
+$querygetAID ="SELECT * FROM Prgr16_Jobs (Email) WHERE AID; FORSTÄTT HÄR IMORGON. 
+        VALUES ('".$email."', '".$job_categpry."', '".$adress."', '".$wage."', '".$est_time."', '".$description."')"; ""
+ $title ='Annonssida3.php';//Måste skapas av sessionsvariabel med namnet Session["AID"]. Ska ta det sista Session["AID"] och lägga till en.
     echo "$title har skapats";
     //the data
-
-    $data = "Hey I am Aidan\n";
+    include 'Include/CreateNewPage.php';
+    $data = CreateNewPage();
 
     //open the file and choose the mode
 
