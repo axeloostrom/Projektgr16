682ef61933b113182d2388078cd5110b2af1cbbc
williamjossi <50870062+williamjossi@users.noreply.github.com>
2019-05-30 10:13:18 +0200

    asdasd

--- AnnonsSida.php
+++ AnnonsSida.php
@@ -5,11 +5,13 @@ $host = "dbtrain.im.uu.se";
 $dbname = "dbtrain_850";	
 //Login-credentials for database	
 
-$connection = new mysqli( $host, $uname, $pass, $dbname);
+$connection = new mysqli($host, $uname, $pass, $dbname);
+
 if ($connection -> connect_error)
 {
-	die ("Connection failed:".$connection.connect_error) ;
+	die ("Connection failed:".$connect_error) ;
 }
+echo "Connection worked!"
 //Displays "Connection failed" on site if Connection error. Otherwise it displays nothing.
 
 $query = "SELECT * FROM Prgr16_Jobs WHERE AID='1'";
